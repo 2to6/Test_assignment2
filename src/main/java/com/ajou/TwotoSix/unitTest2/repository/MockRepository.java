75c158431f66a5d8e92dc8348f82b519a20e7775
jinyoung <bmdj123@gmail.com>
2019-07-17 18:16:01 +0900

    Add findBystudentId method

--- src/main/java/com/ajou/TwotoSix/unitTest2/repository/MockRepository.java
+++ src/main/java/com/ajou/TwotoSix/unitTest2/repository/MockRepository.java
@@ -1,5 +1,10 @@
 package com.ajou.TwotoSix.unitTest2.repository;
 
 
+import com.ajou.TwotoSix.unitTest2.domain.Student;
+
 public interface MockRepository {
+
+    Student findBystudentId(String studentId);
+
 }
--- src/main/java/com/ajou/TwotoSix/unitTest2/service/MockService.java
+++ src/main/java/com/ajou/TwotoSix/unitTest2/service/MockService.java
@@ -1,5 +1,18 @@
 package com.ajou.TwotoSix.unitTest2.service;
 
+import com.ajou.TwotoSix.unitTest2.domain.Student;
+import com.ajou.TwotoSix.unitTest2.repository.MockRepository;
+
 public class MockService {
+    private final MockRepository mockRepository;
+
+    public MockService(MockRepository mockRepository) {
+        this.mockRepository = mockRepository;
+    }
+
+    public Student findBystudentId(String studentId){
+        Student student = mockRepository.findBystudentId(studentId);
+        return student;
+    }
 
 }
