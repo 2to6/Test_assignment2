4536b383705d9412793f06df7d9fa890bc1cbacb
vihwan <vihwan@ajou.ac.kr>
2019-07-17 18:59:27 +0900

    Merge Conflict Resolved

--- src/test/java/com/ajou/TwotoSix/unitTest2/service/MockServiceTest.java
+++ src/test/java/com/ajou/TwotoSix/unitTest2/service/MockServiceTest.java
@@ -1,4 +1,23 @@
 package com.ajou.TwotoSix.unitTest2.service;
 
+import com.ajou.TwotoSix.unitTest2.domain.Student;
+import com.ajou.TwotoSix.unitTest2.repository.MockRepository;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MockServiceTest {
+
+    @Mock
+    private MockRepository mockRepository;
+
+    @InjectMocks
+    private MockService mockService;
+
+
+    private List<Student> students = new ArrayList<>();
+
+
 }
