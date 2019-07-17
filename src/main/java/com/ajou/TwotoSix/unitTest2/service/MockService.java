1016817f9f569ea71f4bcd43cf6e305ec7ef12b3
lang0909 <lang0909@ajou.ac.kr>
2019-07-17 18:35:22 +0900

    Add "addStudent method"

--- src/main/java/com/ajou/TwotoSix/unitTest2/service/MockService.java
+++ src/main/java/com/ajou/TwotoSix/unitTest2/service/MockService.java
@@ -1,5 +1,13 @@
 package com.ajou.TwotoSix.unitTest2.service;
 
+import com.ajou.TwotoSix.unitTest2.domain.Student;
+
 public class MockService {
+
+    public Student addStudent(String name, String studentId, int currentSemester, String major, float GPA){
+        Student addStudent = new Student(name, studentId, currentSemester, major, GPA);
+        return addStudent;
+    }
+
 
 }
