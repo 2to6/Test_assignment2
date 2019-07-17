00235071cfea75637ad1f3e5a9b5976dd904d569
vihwan <vihwan@ajou.ac.kr>
2019-07-17 18:33:26 +0900

    Add @Builder Annotation

--- src/main/java/com/ajou/TwotoSix/unitTest2/domain/Student.java
+++ src/main/java/com/ajou/TwotoSix/unitTest2/domain/Student.java
@@ -2,13 +2,14 @@ package com.ajou.TwotoSix.unitTest2.domain;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 
 @AllArgsConstructor
 @Data
+@Builder
 public class Student {
-
     private String name;
     private String studentId;
     private int currentSemester;
