893d1114917761f6ebf53e0bd99556c8d64eb613
vihwan <vihwan@ajou.ac.kr>
2019-07-17 18:35:11 +0900

    Add deleteStudent & searchStudent Method Use Stream API

--- src/main/java/com/ajou/TwotoSix/unitTest2/service/MockService.java
+++ src/main/java/com/ajou/TwotoSix/unitTest2/service/MockService.java
@@ -1,5 +1,30 @@
 package com.ajou.TwotoSix.unitTest2.service;
 
+import com.ajou.TwotoSix.unitTest2.domain.Student;
+import com.ajou.TwotoSix.unitTest2.repository.MockRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MockService {
+
+    private MockRepository mockRepository;
+
+
+    public Student findByName(String name) {
+        Student studentName = mockRepository.findByName(name);
+        return studentName;
+    }
+
+    public void deleteStudent(String name) {
+        Student deleteStudentName = findByName(name);
+        mockRepository.deleteStudent(deleteStudentName);
+    }
+
+    public List<Student> searchStudentByName(List<Student> studentList, String filterByName) {
+        return studentList.stream()
+                .filter(student -> student.getName().equals(filterByName))
+                .collect(Collectors.toList());
+    }
 
 }
