b0f2445ae3ab67ed062cf8f997e915da0c061219
Choejiwon <jiwon0929@ajou.ac.kr>
2019-07-17 18:33:12 +0900

    Add "updateGPA method and findByStudentId mehtod" in repository package. Implement initial these method.

--- src/main/java/com/ajou/TwotoSix/unitTest2/repository/MockRepository.java
+++ src/main/java/com/ajou/TwotoSix/unitTest2/repository/MockRepository.java
@@ -1,5 +1,11 @@
 package com.ajou.TwotoSix.unitTest2.repository;
 
 
+import com.ajou.TwotoSix.unitTest2.domain.Student;
+
 public interface MockRepository {
+
+    float updateGPA(Student student, float updatedGPA);
+
+    Student findByStudentId(String studentId);
 }
--- src/main/java/com/ajou/TwotoSix/unitTest2/service/MockService.java
+++ src/main/java/com/ajou/TwotoSix/unitTest2/service/MockService.java
@@ -1,5 +1,23 @@
 package com.ajou.TwotoSix.unitTest2.service;
 
-public class MockService {
+import com.ajou.TwotoSix.unitTest2.domain.Student;
+import com.ajou.TwotoSix.unitTest2.repository.MockRepository;
 
+public class MockService {
+    private final MockRepository mockRepository;
+
+    public MockService(MockRepository mockRepository) {
+        this.mockRepository = mockRepository;
+    }
+
+    public float updateGPAByStudentId(String studentId, float updatedGPA) {
+        Student student = findBystudentId(studentId);
+        student.setGPA(updatedGPA);
+        return student.getGPA();
+    }
+
+    public Student findBystudentId(String studentId) {
+        Student student = mockRepository.findByStudentId(studentId);
+        return student;
+    }
 }
