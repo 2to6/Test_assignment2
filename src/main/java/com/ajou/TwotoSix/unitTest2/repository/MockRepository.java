40c13595b7dcb3a048937532b247bf6a5e2e36a4
vihwan <vihwan@ajou.ac.kr>
2019-07-17 18:34:09 +0900

    Update MockRepository Interface

--- src/main/java/com/ajou/TwotoSix/unitTest2/repository/MockRepository.java
+++ src/main/java/com/ajou/TwotoSix/unitTest2/repository/MockRepository.java
@@ -1,5 +1,12 @@
 package com.ajou.TwotoSix.unitTest2.repository;
 
 
+import com.ajou.TwotoSix.unitTest2.domain.Student;
+
 public interface MockRepository {
+
+
+    Student findByName(String name);
+
+    void deleteStudent(Student deleteStudentName);
 }
