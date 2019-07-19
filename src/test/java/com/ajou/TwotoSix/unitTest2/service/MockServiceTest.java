c159665a2867b6d0e12947bd770a72b553317b8d
Choejiwon <jiwon0929@ajou.ac.kr>
2019-07-19 18:58:15 +0900

    Change every Method's name and used method from setCurrentSemester -> updateGPAByStudentId

--- src/test/java/com/ajou/TwotoSix/unitTest2/service/MockServiceTest.java
+++ src/test/java/com/ajou/TwotoSix/unitTest2/service/MockServiceTest.java
@@ -7,7 +7,6 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.ArrayList;
@@ -23,15 +22,15 @@ import static org.mockito.Mockito.*;
 @RunWith(MockitoJUnitRunner.class)
 public class MockServiceTest {
 
-//    @Mock
-//    private MockRepository mockRepository;
-//
-//    @InjectMocks
-//    private MockService mockService;
+    @Mock
+    private MockRepository mockRepository;
+
+    @InjectMocks
+    private MockService mockService;
 
     //mock()
     @Test
-    public void mock_사용_간단한예제(){
+    public void mock메소드_사용했을때_notNull이면_pass(){     //최지원
         Student student = mock(Student.class);
         assertTrue( student != null);
     }
@@ -40,7 +39,7 @@ public class MockServiceTest {
     @Mock
     Student student;
     @Test
-    public void mock_사용_간단예제2(){
+    public void mock어노테이션_사용했을때_notNull이면_pass(){   //최지원
         MockitoAnnotations.initMocks(this);
         //MockitoAnnotations.initMocks(this)를 이용하면 Mockito 어노테이션이 선언된 변수들은 객체를 만들어 낸다.
         assertTrue(student != null);
@@ -48,18 +47,16 @@ public class MockServiceTest {
 
     //when()
     @Test
-    public void when_example(){
-        student = mock(Student.class);
-        when(student.getName()).thenReturn("CJW");
-        when(student.getCurrentSemester()).thenReturn(6);
-        assertTrue("CJW".equals(student.getName()));
-        assertTrue(6 == student.getCurrentSemester());
+    public void when_thenReturn_사용_updateGPAByStudentId_실행했을때_4리턴하면_pass(){  //최지원
+        mockService = mock(MockService.class);
+        when(mockService.updateGPAByStudentId(anyString(),anyDouble())).thenReturn(4.0);
+        assertTrue(mockService.updateGPAByStudentId(anyString(),anyDouble()) == 4.0);
     }
 
     //when() 2nd
     @Test
-    public void when_2_example(){
-        MockService mockService = mock(MockService.class);
+    public void when_thenReturn_사용_List만들었을떄_getList하고_NotNull이면_pass(){    //최지원
+        mockService = mock(MockService.class);
         when(mockService.getList(anyString(),anyInt()))
                 .thenReturn(
                         new ArrayList<String>(){
@@ -69,51 +66,49 @@ public class MockServiceTest {
         assertTrue(mockService.getList(anyString(),anyInt())!=null);
     }
 
-    //https://jdm.kr/blog/222
-
     //doThrow() : 예외 던지기
     //eq() : 특정 값을 넣기
     @Test(expected = IllegalArgumentException.class)
-    public void doThrow_example(){
-        Student student = mock(Student.class);
-        doThrow(new IllegalArgumentException()).when(student).setName(eq("CJW"));
-        String name = "CJW";
-        student.setName(name);
+    public void doThrow_사용했을떄_IllegalArgumentException_throw하면_pass(){  //최지원
+        mockService = mock(MockService.class);
+        doThrow(new IllegalArgumentException()).when(mockService).updateGPAByStudentId(eq("201420997"),anyDouble());
+        //mockService.updateGPAByStudentId(anyString(),4.3);  //fail
+        mockService.updateGPAByStudentId("201420997",4.4);
     }
 
     //doNothing() : void로 선언된 메소드에 when()을 걸고 싶을 때 사용
     @Test
-    public void doNothing_example(){
+    public void doNothing_사용했을때_아무메소드도_실행안됐으면_pass(){   //최지원
         Student student = mock(Student.class);
-        doNothing().when(student).setName(anyString());
-        student.setName("CJW");
-        verify(student).setName(anyString());
-        System.out.println("Name::"+student.getName());
+        doNothing().when(student).setGPA(anyDouble());
+        student.setGPA(4.3);
+        verify(student).setGPA(anyDouble());
+        System.out.println("GPA::"+student.getGPA());
     }
 
     //verify() : 해당 구문이 호출 되었는지를 체크.
     //단순한 호출뿐만 아니라 횟수나 타임아웃 시간까지 지정해서 체크 가능
     @Test
-    public void verify_example(){
+    public void verify사용하여_currentSemester관련_메소드들이_조건에맞게_실행되었으면_pass(){ //최지원
         Student student = mock(Student.class);
-        String name = "CJW";
-        student.setName(name);
+        int currentSemester = 6;
+        student.setCurrentSemester(currentSemester);
         //n번 호출했는지 체크
-        verify(student, times(1)).setName(anyString()); //success
+        verify(student, times(1)).setCurrentSemester(anyInt()); //success
         //호출 안했는지 체크
-        verify(student, never()).getName();     //success
-        verify(student, never()).setName(eq("CJM"));    //success
-        verify(student, never()).setName(eq("CJW"));    //fail
+        verify(student, never()).getCurrentSemester();     //success
+        verify(student, never()).setCurrentSemester(eq(10));    //success
+        verify(student, never()).setCurrentSemester(eq(6));    //fail
         //최소 1번 이상 호출했는지 체크
-        verify(student,atLeastOnce()).setName(anyString());     //success
+        verify(student,atLeastOnce()).setCurrentSemester(anyInt());     //success
         //2번 이하 호출했는지 체크
-        verify(student, atMost(2)).setName(anyString());      //success
+        verify(student, atMost(2)).setCurrentSemester(anyInt());      //success
         //2번 이상 호출했는지 체크
-        verify(student,atLeast(2)).setName(anyString());      //fail
+        verify(student,atLeast(2)).setCurrentSemester(anyInt());      //fail
         //지정된 시간(millis)안으로 메소드를 호출했는지 체크
-        verify(student,timeout(100)).setName(anyString());      //success
+        verify(student,timeout(100)).setCurrentSemester(anyInt());      //success
         //지정된 시간안으로 1번 이상 메소드를 호출했는지 체크
-        verify(student,timeout(100).atLeastOnce()).setName(anyString());        //success
+        verify(student,timeout(100).atLeastOnce()).setCurrentSemester(anyInt());        //success
     }
 
     //@InjectMocks: 클래스 내부에 다른 클래스를 포함하는 경우에 사용
@@ -121,36 +116,14 @@ public class MockServiceTest {
     //@InjectMocks annotations을 사용하여 @Mock이나 @Spy 어노테이션이 붙은 mock 객체를 자신의 멤버 클래스와 일치하여
     //주입시킨다.
 
-    @Mock
-    MockRepository mockRepository;
-
-    @InjectMocks
-    MockService mockService;
-
     @Test   //BDD 스타일
-    public void example_InjectMocks(){
+    public void BDD_스타일로_updateGPAByStudentId_메소드_Test(){
         //given : 선행 조건 기술
-        given(mockRepository.findByName(eq("CJW"))).willReturn(new Student("CJW","201420997",6,"Software",3.6));
+        given(mockRepository.updateGPAByStudentId(eq("201420997"),eq(4.0))).willReturn(new Student("CJW","201420997",6,"Software",4.0).getGPA());
         //when : 기능 수행
-        Student student = mockService.findByName("CJW");
-        student = mockService.findByName("CJW");
+        double GPAofStudent = mockRepository.updateGPAByStudentId("201420997",4.0);
         //then : 결과 확인
-        verify(mockRepository, times(2)).findByName("CJW");
-        assertThat(student.getGPA(), is(3.6));
-
-        when(mockRepository.findByName("CJW")).thenReturn(new Student("CJM","201712345",5,"sooeui",3.5));
-        assertTrue(mockService.findBystudentId("201712345") == new Student("CJM","201712345",5,"sooeui",3.5));
+        verify(mockRepository, times(1)).updateGPAByStudentId("201420997",4.0);
+        assertThat(GPAofStudent, is(4.0));
     }
-
-    //@Spy : @Spy로 선언된 mock 객체는 mock method(stub)를 별도로 만들지 않는다면 실제 method가 호출된다.
-    //이건 어떻게 쓰는지 모르겠다 ㅠㅠ
-//    @Test
-//    public void example_spy(){
-//        MockService mockService = spy(MockService.class);
-//        doReturn("CJW").when(student).getName();
-//        assertTrue(student.getName() == "CJW");
-//    }
-
-
-
 }
