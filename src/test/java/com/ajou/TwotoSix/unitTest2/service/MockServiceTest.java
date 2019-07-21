cb6e1416d7d4d275dfa7da02e42b5c121d928bdc
do02reen24 <do02reen24@gmail.com>
2019-07-21 10:06:12 +0900

    GPA가 기준미달일 때 false를 return 하는지 Test

--- src/test/java/com/ajou/TwotoSix/unitTest2/service/MockServiceTest.java
+++ src/test/java/com/ajou/TwotoSix/unitTest2/service/MockServiceTest.java
@@ -2,12 +2,19 @@ package com.ajou.TwotoSix.unitTest2.service;
 
 import com.ajou.TwotoSix.unitTest2.domain.Student;
 import com.ajou.TwotoSix.unitTest2.repository.MockRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
 public class MockServiceTest {
 
     @Mock
@@ -16,8 +23,18 @@ public class MockServiceTest {
     @InjectMocks
     private MockService mockService;
 
-
     private List<Student> students = new ArrayList<>();
+
+    @Test
+    public void GPA가_한번설정되고_기준미달일때_false인지_확인(){ //김도연
+        Student student = mock(Student.class);
+        student.setGPA(3.49);
+        verify(student, times(1)).setGPA(anyDouble());
+        boolean result = mockService.ScholarshipVaild(student);
+        assertTrue( result == false);
+
+    }
+
 
 
 }
