ddbfbad10ddca0b0896886ea4986e91c64bfb29c
do02reen24 <do02reen24@gmail.com>
2019-07-21 10:49:03 +0900

    GPA가 기준충족일 때 true를 return 하는지 Test

--- src/test/java/com/ajou/TwotoSix/unitTest2/service/MockServiceTest.java
+++ src/test/java/com/ajou/TwotoSix/unitTest2/service/MockServiceTest.java
@@ -26,15 +26,33 @@ public class MockServiceTest {
     private List<Student> students = new ArrayList<>();
 
     @Test
-    public void GPA가_한번설정되고_기준미달일때_false인지_확인(){ //김도연
+    public void GPA가_기준미달일때_false인지_테스트(){ //김도연
         Student student = mock(Student.class);
-        student.setGPA(3.49);
-        verify(student, times(1)).setGPA(anyDouble());
+        when(student.getGPA()).thenReturn(3.49);
         boolean result = mockService.ScholarshipVaild(student);
         assertTrue( result == false);
-
     }
 
+    @Test
+    public void GPA가_기준충족일때_true인지_테스트() { //김도연
+        Student student = mock(Student.class);
+        when(student.getGPA()).thenReturn(3.5);
+        boolean result = mockService.ScholarshipVaild(student);
+        assertTrue(result == true);
+    }
+
+
+    @Test
+    public void GPA_업데이트후_결과반영이_잘되는지_테스트(){ //김도연
+        Student student = mock(Student.class);
+        student.setGPA(3.2);
+        student.setStudentId("201720711");
+        mockService.updateGPAByStudentId("201720711", 3.5);
+        verify(mockService, times(1)).updateGPAByStudentId(anyString(), anyDouble());
+        boolean result = mockService.ScholarshipVaild(student);
+        System.out.println(result);
+        //assertTrue(result == true);
+    }
 
 
 }
